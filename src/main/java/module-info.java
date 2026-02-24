1517a5a4df8de2554df3fdd1a3cccd996b13d089
SF-45 <akcaynt1@gmail.com>
2026-02-24 19:20:00 +0700

    BREAKING CHANGE: Наведение порядка. Управление зависимостями через owlook-bom. SemVer + Conventional Commits.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module owlook.base {
-	exports space.sadfox.owlook.base.jaxb;
-	exports space.sadfox.owlook.base.moduleapi;
-	exports space.sadfox.owlook.base.owl;
-	
-	opens space.sadfox.owlook.base.moduleapi to jakarta.xml.bind;
-	opens space.sadfox.owlook.base.owl to jakarta.xml.bind;
+  exports space.sadfox.owlook.base.jaxb;
+  exports space.sadfox.owlook.base.moduleapi;
+  exports space.sadfox.owlook.base.owl;
 
-	requires transitive jakarta.xml.bind;
-	requires transitive javafx.base;
+  opens space.sadfox.owlook.base.moduleapi to jakarta.xml.bind;
+  opens space.sadfox.owlook.base.owl to jakarta.xml.bind;
+
+  requires transitive jakarta.xml.bind;
+  requires transitive javafx.base;
+  requires org.slf4j;
 }
